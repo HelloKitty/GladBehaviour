56bb9f16ef765de2fcf3bc269952b5815f47fa3c
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-21 21:42:56 -0600

    Structures used to store data

---
+++ src/GladBehaviour.Common/DataStores/CollectionComponentDataStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GladBehaviour.Common
+{
+	[Serializable]
+	public class CollectionComponentDataStore : IEnumerable<SingleComponentDataStore>
+	{
+		[SerializeField]
+		List<SingleComponentDataStore> dataStoreCollection;
+
+		public CollectionComponentDataStore()
+		{
+			dataStoreCollection = new List<SingleComponentDataStore>();
+		}
+
+		public CollectionComponentDataStore(int initialSize)
+		{
+			dataStoreCollection = new List<SingleComponentDataStore>(initialSize);
+		}
+
+		public CollectionComponentDataStore(IEnumerable<SingleComponentDataStore> data)
+		{
+			dataStoreCollection = data.ToList();
+		}
+
+		public IEnumerator<SingleComponentDataStore> GetEnumerator()
+		{
+			return dataStoreCollection?.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return dataStoreCollection?.GetEnumerator();
+		}
+	}
+}
---
+++ src/GladBehaviour.Common/DataStores/SingleComponentDataStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GladBehaviour.Common
+{
+	[Serializable]
+	public class SingleComponentDataStore
+	{
+		[SerializeField]
+		public readonly UnityEngine.Component StoredComponent;
+
+		public SingleComponentDataStore(UnityEngine.Component com)
+		{
+			StoredComponent = com;
+		}
+	}
+}
