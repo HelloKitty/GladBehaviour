fbf2c4d79f48c39657c70733fd465060a735bee2
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-21 21:58:57 -0600

    Added fields required to serialize data

--- src/GladBehaviour.Common/GladMonoBehaviour.cs
+++ src/GladBehaviour.Common/GladMonoBehaviour.cs
@@ -8,16 +8,22 @@ namespace GladBehaviour.Common
 {
 	public class GladMonoBehaviour : MonoBehaviour, ISerializationCallbackReceiver
 	{
+		[SerializeField]
+		[SingleCollectionSerialization]
+		private List<SingleComponentDataStore> singleDataStoreCollection;
 
+		[SerializeField]
+		[SingleCollectionSerialization]
+		private List<CollectionComponentDataStore> collectionDataStoreCollection;
 
 		public void OnAfterDeserialize()
 		{
-			throw new NotImplementedException();
+			//throw new NotImplementedException();
 		}
 
 		public void OnBeforeSerialize()
 		{
-			throw new NotImplementedException();
+			//throw new NotImplementedException();
 		}
 	}
 }
