5466af78bc1b044ba9086d548bf233dd6782ef83
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-21 21:35:41 -0600

    Metadata Markers
    These metadata markets indicate where the data is serialized.

---
+++ src/GladBehaviour.Common/Attributes/ListCollectionSerializationAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladBehaviour.Common
+{
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class ListCollectionSerializationAttribute : Attribute
+	{
+
+	}
+}
---
+++ src/GladBehaviour.Common/Attributes/SingleCollectionSerializationAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladBehaviour.Common.Attributes
+{
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class SingleCollectionSerializationAttribute : Attribute
+	{
+
+	}
+}
