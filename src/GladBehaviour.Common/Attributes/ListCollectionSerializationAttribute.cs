5fda05f36cdf619d9332f34c6d8ef3d7214f1fe4
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-23 16:55:10 -0600

    Document Metadata Classes

--- src/GladBehaviour.Common/Attributes/ListCollectionSerializationAttribute.cs
+++ src/GladBehaviour.Common/Attributes/ListCollectionSerializationAttribute.cs
@@ -5,9 +5,12 @@ using System.Text;
 
 namespace GladBehaviour.Common
 {
+	/// <summary>
+	/// Meta-data marker for a serializable list of collection containers
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
 	public class ListCollectionSerializationAttribute : Attribute
 	{
-
+		//we don't need any properties
 	}
 }
