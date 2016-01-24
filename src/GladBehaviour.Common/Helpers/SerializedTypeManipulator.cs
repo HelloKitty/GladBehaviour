de72f6b0f5d63b6db33425c64a265e1a1f6cc6c5
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-24 01:56:42 -0600

    Added NUnit and Moq

---
+++ src/GladBehaviour.Common/Helpers/SerializedTypeManipulator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladBehaviour.Common
+{
+	//Unity doesn't really allow us to inject things so
+	//the next best thing to do is sadly make things static
+	public static class SerializedTypeManipulator
+	{
+		public static bool isInterfaceCollectionType(Type type)
+		{
+			return type != null && type.IsGenericType && typeof(IEnumerable).IsAssignableFrom(type) && !type.GetGenericArguments().First().IsPrimitive;
+		}
+	}
+}
