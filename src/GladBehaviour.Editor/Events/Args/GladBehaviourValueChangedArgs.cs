a1dab3a0a2917b1e6fd2ba9811efea60ef8afc0c
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-23 20:19:47 -0600

    Document change event

--- src/GladBehaviour.Editor/Events/Args/GladBehaviourValueChangedArgs.cs
+++ src/GladBehaviour.Editor/Events/Args/GladBehaviourValueChangedArgs.cs
@@ -6,8 +6,14 @@ using System.Text;
 
 namespace GladBehaviour.Editor
 {
+	/// <summary>
+	/// Arguments for a value changing event.
+	/// </summary>
 	public class GladBehaviourValueChangedArgs : EventArgs
 	{
+		/// <summary>
+		/// Thew new changed value.
+		/// </summary>
 		public readonly object ChangedValue;
 
 		public GladBehaviourValueChangedArgs(object newValue)
--- src/GladBehaviour.Editor/Events/OnValueChanged.cs
+++ src/GladBehaviour.Editor/Events/OnValueChanged.cs
@@ -5,5 +5,10 @@ using System.Text;
 
 namespace GladBehaviour.Editor
 {
+	/// <summary>
+	/// Action signature for events related to behaviour component values changing.
+	/// </summary>
+	/// <param name="sender">Object that raised the event.</param>
+	/// <param name="args">Change arguments.</param>
 	public delegate void OnValueChanged(object sender, GladBehaviourValueChangedArgs args);
 }
