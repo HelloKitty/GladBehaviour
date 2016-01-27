f1c77aeffcbe65a5ab7ec72469d1fdc05dc33f9e
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-26 23:19:31 -0600

    Refactored container updating code

---
+++ src/GladBehaviour.Common/Helpers/ContainerParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Fasterflect;
+
+namespace GladBehaviour.Common
+{
+	public class ContainerParser<TSerializedContainerType>
+		where TSerializedContainerType : ISerializableContainer
+    {
+		private readonly IEnumerable<TSerializedContainerType> containerCollection;
+
+		private readonly IContainerFieldMatcher matchStrategy;
+
+		public ContainerParser(IEnumerable<TSerializedContainerType> collection, IContainerFieldMatcher matchStrat)
+		{
+			if (collection == null)
+				throw new ArgumentNullException(nameof(collection), "Cannot parse a null " + nameof(ISerializableContainer) + " collection.");
+
+			if (matchStrat == null)
+				throw new ArgumentNullException(nameof(matchStrat), "Cannot parse container collection with a null " + nameof(IContainerFieldMatcher));
+
+			containerCollection = collection;
+			matchStrategy = matchStrat;
+        }
+
+		public IEnumerable<ISerializableContainer> ComputeStaleContainers()
+		{
+			List<ISerializableContainer> staleFields = new List<ISerializableContainer>(containerCollection.Count()); //Too large an allocation is probably better than multiple
+
+			//if it's empty we don't need to do searches
+			if (containerCollection.Count() == 0)
+				return staleFields;
+
+			foreach (ISerializableContainer con in containerCollection)
+			{
+				//If it doesn't have a match we should
+				//add it to the collection so it can be removed
+				if (!matchStrategy.hasMatch(con))
+					staleFields.Add(con);
+			}
+
+			return staleFields;
+		}
+
+		private IEnumerable<FieldInfo> FindNewCollectionMembers()
+		{
+			return matchStrategy.FindUnContainedFields(containerCollection.Cast<ISerializableContainer>());
+		}
+	}
+}
---
+++ src/GladBehaviour.Common/Helpers/IContainerFieldMatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace GladBehaviour.Common
+{
+	public interface IContainerFieldMatcher
+	{
+		bool hasMatch(ISerializableContainer container);
+
+		FieldInfo FindMatch(ISerializableContainer container);
+
+		IEnumerable<FieldInfo> FindUnContainedFields(IEnumerable<ISerializableContainer> containers);
+	}
+}
