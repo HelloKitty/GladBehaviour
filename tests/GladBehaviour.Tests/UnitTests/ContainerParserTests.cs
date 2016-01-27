83a15be2620d5f0cf755bb69106e84a3739729e9
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-26 23:19:42 -0600

    Added ContainerParser tests

---
+++ tests/GladBehaviour.Tests/UnitTests/ContainerParserTests.cs
@@ -0,0 +1,22 @@
+﻿using GladBehaviour.Common;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladBehaviour.Tests.UnitTests
+{
+	[TestFixture]
+	public static class ContainerParserTests
+	{
+		[Test(Author = "Andrew Blakely", Description = "Verifies that an unexpected type is not considered interface collection type.", TestOf = typeof(SerializedTypeManipulator))]
+		public static void Test_ContainerParser_Contructor_Throws_On_Null_Dependencies()
+		{
+			//assert
+			Assert.Throws<ArgumentNullException>(() => new ContainerParser<ISerializableContainer>(null, Mock.Of<IContainerFieldMatcher>()));
+			Assert.Throws<ArgumentNullException>(() => new ContainerParser<ISerializableContainer>(Mock.Of<IEnumerable<ISerializableContainer>>(), null));
+		}
+	}
+}
