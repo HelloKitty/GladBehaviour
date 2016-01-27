55acc3f307bb46e74b0396098e78512713c849b1
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-27 00:29:41 -0600

    Added NetEssentials Dependency

---
+++ lib/BuildDependencies.bat
@@ -0,0 +1,2 @@
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
+xcopy  /R /E /Y /q ".\Net35Essentials\src\Net35Essentials\bin\Release" ".\Dependency Builds\Net35Essentials\DLLs\"
